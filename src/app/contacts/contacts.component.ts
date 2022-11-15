cb677c90d531a6c7b7dec25d550987a4a018e779
file098 <filippodigennaro98@outlook.it>
2022-11-15 15:48:27 +0100

    i think everything looks good

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {} // the element that you are observing (just add #yourElement in the template) for this  query to work
 
-  ngOnInit(): void {
+  @ViewChild('contacts') yourElement: ElementRef | undefined;
+
+  ngAfterViewInit() {
+    const threshold = 0.1; // how much % of the element is in view
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let cardsCollection: NodeListOf<HTMLElement> = document.querySelectorAll('#vertical-contacts p');
+            cardsCollection.forEach((p, index) => {
+              p.classList.add('fadeIn');
+              p.style.animationDelay = index/1.5 + "s";
+            })
+            observer.disconnect(); // disconnect if you want to stop observing else it will rerun every time its back in view. Just make sure you disconnect in ngOnDestroy instead
+          }
+        });
+      },
+      { threshold }
+    );
+    observer.observe(this.yourElement!.nativeElement);
   }
-
 }
